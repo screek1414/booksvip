e61d91bc0f031f9fb1efb2f3d188d431a53e1f7a
Viktor Semenov <kq000@yandex.ru>
2019-08-15 15:33:37 +0300

    Ref

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -59,8 +59,8 @@ class LoginController extends Controller
         }
 
         // check if they're an existing user
-        dd(AuthUser::where('email', $user->email)->first());
         $existingUser = AuthUser::where('email', $user->email)->first();
+        dd(AuthUser::where('email', $user->email)->first());
         if ($existingUser) {
             // log them in
             auth()->login($existingUser, true);
