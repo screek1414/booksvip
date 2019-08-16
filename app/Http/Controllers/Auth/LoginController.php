a8aa8d1ad5d57e4547030b70a2b9b5d1b0971d78
Viktor Semenov <kq000@yandex.ru>
2019-08-16 10:58:39 +0300

    Debug

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use mysql_xdevapi\Exception;
 
 class LoginController extends Controller
 {
@@ -34,6 +35,10 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        try {
+            $this->middleware('guest')->except('logout');
+        } catch (Exception $exception) {
+            dd($exception);
+        }
     }
 }
