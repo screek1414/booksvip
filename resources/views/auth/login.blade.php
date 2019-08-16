9961737462a5205f095c143f2a5b2d2975ed624c
Viktor Semenov <kq000@yandex.ru>
2019-08-16 11:27:47 +0300

    Debug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-                                <a href="{{ __('/redirect') }}"> asdasdasd </a>
+                                <a href="{{ route('google') }}"> asdasdasd </a>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
