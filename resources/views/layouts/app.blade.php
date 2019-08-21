c8ca090ea18310a25c7b4f7189d0b37b91dbac21
Viktor Semenov <kq000@yandex.ru>
2019-08-21 18:21:25 +0300

    restyle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,10 @@
                         </li>
                     @endif
                 @else
-                    <a id="navbarDropdown" class="header-link dropdown-toggle" href="#" role="button"
+                    <li id="navbarDropdown" class="header-link dropdown-toggle" href="#" role="button"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
+                    </li>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('logout') }}"
                            onclick="event.preventDefault();
