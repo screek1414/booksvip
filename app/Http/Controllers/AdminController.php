380cc8acd5872ca8696b6b6197363e91901bef96
Viktor Semenov <kq000@yandex.ru>
2019-08-22 11:48:58 +0300

    Admin page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,7 +8,7 @@ class AdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('admin')
+        $this->middleware('admin');
     }
 
     public function index()
